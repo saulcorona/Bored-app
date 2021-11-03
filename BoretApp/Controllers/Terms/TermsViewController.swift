4829f2f88cb60fec7335d3053ad2f20d4d0fc1f1
Sebastian San Blas <sebastian.sanblas@mercadolibre.com>
2021-11-03 17:54:33 -0300

    TermsViewController done

--- BoretApp/Controllers/Terms/TermsViewController.swift
+++ BoretApp/Controllers/Terms/TermsViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class TermsViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.textView.text = load(file: "terms")
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissTermsTapped(_ sender: Any) {
+        print("dismiss work!")
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension TermsViewController {
+    
+    private func load(file name:String) -> String {
+        if let path = Bundle.main.path(forResource: name, ofType: nil) {
+            if let contents = try? String(contentsOfFile: path) {
+                return contents
+            } else {
+                print("Error! - This file doesn't contain any text.")
+                ProgressHUD.showError("Ups, something went wrong :(", image: nil, interaction: true)
+            }
+        } else {
+            ProgressHUD.showError("Ups, something went wrong :(", image: nil, interaction: true)
+        }
+        return ""
+    }
+    
+    private func setupUI() {
+        closeButton.tintColor = UIColor(red: 34/255, green: 229/255, blue: 171/255, alpha: 1)
+        titleLabel.textColor = UIColor(red: 34/255, green: 229/255, blue: 171/255, alpha: 1)
+    }
 }
--- BoretApp/SupportFiles/AppDelegate.swift
+++ BoretApp/SupportFiles/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        configProgressHUD()
         return true
     }
 
@@ -30,7 +31,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate {
+    
+    func configProgressHUD() {
+        ProgressHUD.animationType = .circleStrokeSpin
+        ProgressHUD.colorBackground = .clear
+        ProgressHUD.colorHUD = UIColor(red: 34/255, green: 229/255, blue: 171/255, alpha: 1)
+        ProgressHUD.colorAnimation = UIColor(red: 34/255, green: 229/255, blue: 171/255, alpha: 1)
+    }
+}
