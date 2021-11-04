0d8cdb1325ba960b79a95817ee5bcb0f6acd6682
sausantos <saul.santos@mercadolibre.com.mx>
2021-11-03 18:58:13 -0600

    Connect initialView with terms, also add elements to initial view

--- BoretApp/Controllers/Initial/InitialViewController.swift
+++ BoretApp/Controllers/Initial/InitialViewController.swift
@@ -12,18 +12,18 @@ class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToTermsConditions(_ sender: UIButton) {
+        goToTermsController()
     }
-    */
+}
 
+extension InitialViewController{
+    private func goToTermsController(){
+        let vc = TermsViewController(nibName: "TermsViewController", bundle: nil)
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .coverVertical
+        self.present(vc, animated: true, completion: nil)
+    }
 }
