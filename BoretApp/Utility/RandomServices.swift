8a3e714a30ba2bbe46bda33771d22fdbd4163d8b
Sebastian San Blas <sebastian.sanblas@mercadolibre.com>
2021-11-04 12:38:37 -0300

    Services: Call to API - RandomServices and TypeServices are in charge of decoding the json of the service and pass to Struct type called "Activity"

---
+++ BoretApp/Utility/RandomServices.swift
@@ -0,0 +1,40 @@
+//
+//  RandomServices.swift
+//  BoretApp
+//
+//  Created by Sebastian San Blas on 04/11/2021.
+//
+
+import Foundation
+import ProgressHUD
+
+class RandomServices {
+    
+    func getRandomActivity(for participants: Int?, completion: @escaping ([Activity]) -> Void) {
+        ProgressHUD.show()
+        let randomURL = Services.activityRandom(participants: participants)
+        Services.getServices(url: randomURL) { response in
+            switch response {
+            case .success(let data):
+                do {
+                    guard data != nil else {
+                        completion([])
+                        ProgressHUD.showError()
+                        return
+                    }
+                    let activity = try JSONDecoder().decode(Activity.self, from: data!)
+                    ProgressHUD.dismiss()
+                    completion([activity])
+                } catch {
+                    ProgressHUD.showError("Falló.", image: nil, interaction: true)
+                    completion([])
+                }
+            case .failure(let error):
+                print(error)
+                ProgressHUD.showError("Se agotó el tiempo de espera.", image: nil, interaction: true)
+                completion([])
+            }
+        }
+    }
+}
+
---
+++ BoretApp/Utility/TypeServices.swift
@@ -0,0 +1,41 @@
+//
+//  TypeServices.swift
+//  BoretApp
+//
+//  Created by Sebastian San Blas on 04/11/2021.
+//
+
+import Foundation
+import ProgressHUD
+
+class TypeServices {
+    
+    func getTypeActivity(for participants: Int?,
+                         with type: String,
+                         completion: @escaping ([Activity]) -> Void) {
+        ProgressHUD.show()
+        let typeURL = Services.activityType(participants: participants, type: type)
+        Services.getServices(url: typeURL) { response in
+            switch response {
+            case .success(let data):
+                do {
+                    guard data != nil else {
+                        completion([])
+                        ProgressHUD.showError()
+                        return
+                    }
+                    let activity = try JSONDecoder().decode(Activity.self, from: data!)
+                    ProgressHUD.dismiss()
+                    completion([activity])
+                } catch {
+                    ProgressHUD.showError("Falló.", image: nil, interaction: true)
+                    completion([])
+                }
+            case .failure(let error):
+                print(error)
+                ProgressHUD.showError("Se agotó el tiempo de espera.", image: nil, interaction: true)
+                completion([])
+            }
+        }
+    }
+}
