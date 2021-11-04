78f82d63d046e425758c0d9a9ba75a3ea2a5b527
sausantos <saul.santos@mercadolibre.com.mx>
2021-11-03 20:35:27 -0600

    Add functionality to start button & refactor view

--- BoretApp/Controllers/Initial/InitialViewController.swift
+++ BoretApp/Controllers/Initial/InitialViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 
-class InitialViewController: UIViewController {
+class InitialViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        animateKeyboard()
+        userTextField.delegate = self
+        userTextField.placeholder = "Number of participants 1 to 8"
+        userTextField.textAlignment = .center
+        userTextField.layer.borderWidth = 1
+        userTextField.layer.cornerRadius = 4.0
     }
 
     @IBAction func goToTermsConditions(_ sender: UIButton) {
         goToTermsController()
+    }
+    
+    
+    @IBAction func startGameBored(_ sender: Any) {
+        if userTextField.hasText {
+            print("Works")
+        } else {
+            showAlert(title: "You have to enter a number ☹️",
+                message: nil,
+                actions: ["OK"])
+        }
     }
 }
 
@@ -25,5 +43,32 @@ extension InitialViewController{
         vc.modalPresentationStyle = .fullScreen
         vc.modalTransitionStyle = .coverVertical
         self.present(vc, animated: true, completion: nil)
+    }
+}
+
+// MARK: Keyboard Configuration
+extension InitialViewController {
+    func animateKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+}
+
+extension InitialViewController {
+    func showAlert(title: String, message: String?, actions: [String]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions.forEach { value in
+            alert.addAction(UIAlertAction(title: value, style: .default, handler: nil))
+        }
+        self.present(alert, animated: true)
     }
 }
